28e144421b53378d7a150e6a350b12cc50754ac3
Gege-max <ondzegermain15@gmail.com>
2026-02-11 23:12:32 +0000

    Update CORS origins for production deployment
    Removed local origins and added production origin.

--- app/main.py
+++ app/main.py
@@ -11,9 +11,7 @@ app = FastAPI(
 app.include_router(soumission.router)
 app.include_router(admin.router)
 origins = [
-    "http://localhost:5173",
-    "http://localhost:3001",
-    "http://127.0.0.1:3000",
+    https://yabisso242.org/
 ]
 app.add_middleware(
     CORSMiddleware,
